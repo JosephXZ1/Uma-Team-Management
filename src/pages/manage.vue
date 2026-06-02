e2bb165c76c2672500a1bc68fba70bd21fe6cb9f
JosephXZ1 <greninjamaster04@hotmail.com>
2026-06-02 05:17:12 -0500

    Avance en manage.vue : maquetado primera parte

--- src/pages/manage.vue
+++ src/pages/manage.vue
@@ -1,3 +1,105 @@
+<script setup>
+import Boton from '@/components/button.vue'
+</script>
+
 <template>
-    En Madrid
+    <main class="flex flexColumn">
+        <div class="head flex">
+            <h2>Team Spica</h2>
+            <Boton
+                texto="Cambiar nombre"
+                @click-accion="cambiarNombre"
+                color="blanco"
+                :style="{'--btnPadding' : '1rem 1.5rem', '--btnFontSize' : '1.5rem'} "
+            />
+            <Boton
+                texto="Disolver equipo"
+                @click-accion="eliminarEquipo"
+                color="rojo"
+                :style="{'--btnPadding' : '1rem 1.5rem', '--btnFontSize' : '1.5rem'} "
+            />
+        </div>
+
+        <div class="characterCont flex">
+            <div class="characterCont-card flex flexColumn transition">
+                <img src="@/assets/Multimedia/Imagenes/specialweek_02_2.png" alt="">
+                <Boton
+                    texto="Ver ficha"
+                    @click-accion="verFicha"
+                    :style="{'--btnWidth' : '100%', '--btnPadding' : '1rem 1.5rem', '--btnFontSize' : '1.5rem'}"
+                />
+            </div>
+            <div class="characterCont-card flex flexColumn transition">
+                <img src="@/assets/Multimedia/Imagenes/silencesuzuka_02.png" alt="">
+                <Boton
+                    texto="Ver ficha"
+                    @click-accion="verFicha"
+                    :style="{'--btnWidth' : '100%', '--btnPadding' : '1rem 1.5rem', '--btnFontSize' : '1.5rem'}"
+                />
+            </div>
+            <div class="characterCont-card flex flexColumn transition">
+                <img src="@/assets/Multimedia/Imagenes/tokaiteio_02.png" alt="">
+                <Boton
+                    texto="Ver ficha"
+                    @click-accion="verFicha"
+                    :style="{'--btnWidth' : '100%', '--btnPadding' : '1rem 1.5rem', '--btnFontSize' : '1.5rem'}"
+                />
+            </div>
+            <div class="characterCont-card flex flexColumn transition">
+                <img src="@/assets/Multimedia/Imagenes/mejiromcqueen_02.png" alt="">
+                <Boton
+                    texto="Ver ficha"
+                    @click-accion="verFicha"
+                    :style="{'--btnWidth' : '100%', '--btnPadding' : '1rem 1.5rem', '--btnFontSize' : '1.5rem'}"
+                />
+            </div>
+        </div>
+    </main>
 </template>
+
+<style scoped>
+main
+{
+    width: 100%;
+    height: 100vh;
+    padding: 3rem 6.5rem 1.5rem;
+    justify-content: flex-start;
+    gap: 2rem;
+}
+
+.head
+{
+    gap: 1rem;
+}
+
+.head h2
+{
+    font-size: 4rem;
+}
+
+.characterCont
+{
+    width: 100%;
+    height: 75%;
+    gap: 2rem;
+
+    /* background-color: aquamarine; */
+}
+
+.characterCont-card
+{
+    height: 100%;
+    padding: 1rem 1rem;
+    flex: 1;
+    justify-content: space-between;    
+    background-color: #f9f9f9;
+    border-radius: 1.5rem;
+    border: 2px solid #c2bfc3;
+
+    box-shadow: 0 5px 7px transparent
+}
+
+.characterCont-card:hover{ box-shadow: 0 5px 7px #a8a6b7;}
+
+.characterCont-card img  {height: clamp(232px,53vh,518px);}
+</style>
